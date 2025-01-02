0011193d86ce775c8b9aa3e3d58a68c023f51835
mariadonnadelossantos19 <102678677+Mariadonnadelossantos19@users.noreply.github.com>
2025-01-02 09:59:31 +0800

    Mitigate the Risk of Cross Site Scripting (XSS) Attacks with helmet.xssFilter()

--- myApp.js
+++ myApp.js
@@ -2,63 +2,7 @@ const helmet = require('helmet');
 
 const express = require('express');
 const app = express();
+// use helmet middleware to  hide "x-powered-by"
 app.use(helmet.hidePoweredBy());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+app.use(helmet.frameguard({action:'deny'}));
+app.use(helmet.xssFilter());
